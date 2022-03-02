c6ed45117ac6e9cec7a7d4607e8692c1fa815eb4
Dimitar Iliev <info@dimitarsi.me>
2022-03-02 09:15:23 +0100

    feat(calculator): drawing outer wall and door

--- lib/painter/room_painter.dart
+++ lib/painter/room_painter.dart
@@ -6,6 +6,69 @@ import 'dart:math';
 
 import 'package:path/path.dart';
 
+class RoomOffset {
+  double leftStart;
+  double leftEnd;
+  double topStart;
+  double topEnd;
+  double innerOffset;
+
+  RoomOffset(
+      {required this.leftEnd,
+      required this.leftStart,
+      required this.topEnd,
+      required this.topStart,
+      this.innerOffset = 5});
+
+  Offset get topLeft {
+    return Offset(leftStart, topStart);
+  }
+
+  Offset get topRight {
+    return Offset(leftEnd, topStart);
+  }
+
+  Offset get bottomLeft {
+    return Offset(leftStart, topEnd);
+  }
+
+  Offset get bottomRight {
+    return Offset(leftEnd, topEnd);
+  }
+
+  double get leftInnerStart {
+    return leftStart + innerOffset;
+  }
+
+  double get leftInnerEnd {
+    return leftEnd - innerOffset;
+  }
+
+  double get topInnerStart {
+    return topStart + innerOffset;
+  }
+
+  double get topInnerEnd {
+    return topEnd - innerOffset;
+  }
+
+  Offset get topInnerLeft {
+    return Offset(leftStart + innerOffset, topStart + innerOffset);
+  }
+
+  Offset get topInnerRight {
+    return Offset(leftEnd - innerOffset, topStart + innerOffset);
+  }
+
+  Offset get bottomInnerLeft {
+    return Offset(leftStart + innerOffset, topEnd - innerOffset);
+  }
+
+  Offset get bottomInnerRight {
+    return Offset(leftEnd - innerOffset, topEnd - innerOffset);
+  }
+}
+
 class RoomPainter extends CustomPainter {
   int sideA;
   int sideB;
@@ -22,11 +85,151 @@ class RoomPainter extends CustomPainter {
       required this.style,
       required this.canvasWidth});
 
+  double getDoorLength() {
+    int maxSizeWidth = 250;
+    double scaling = min(maxSizeWidth / sideB, maxSizeWidth / sideA);
+    double doorLength = 80 * scaling;
+
+    var scaledSideA = sideA * scaling;
+
+    return min(doorLength, scaledSideA * .5);
+  }
+
+  double getScaledSideA() {
+    int maxSizeWidth = 250;
+    double scaling = min(maxSizeWidth / sideB, maxSizeWidth / sideA);
+    double doorLength = 80 * scaling;
+
+    return sideA * scaling;
+  }
+
+  double getScaledSideB() {
+    int maxSizeWidth = 250;
+    double scaling = min(maxSizeWidth / sideB, maxSizeWidth / sideA);
+    double doorLength = 80 * scaling;
+
+    return sideB * scaling;
+  }
+
+  RoomOffset getRoomOffset() {
+    int maxSizeWidth = 250;
+    double scaling = min(maxSizeWidth / sideB, maxSizeWidth / sideA);
+    double doorLength = 80 * scaling;
+
+    var scaledSideA = sideA * scaling;
+    var scaledSideB = sideB * scaling;
+
+    doorLength = min(doorLength, scaledSideA * .5);
+
+    var leftStart = (canvasWidth - scaledSideA) * .5;
+    var leftEnd = leftStart + scaledSideA;
+
+    var topStart = (canvasHeight - scaledSideB) * .5;
+    var topEnd = topStart + scaledSideB;
+
+    return RoomOffset(
+        leftEnd: leftEnd,
+        leftStart: leftStart,
+        topEnd: topEnd,
+        topStart: topStart);
+  }
+
+  void _drawOuterWalls(Canvas canvas, Paint paint) {
+    var offset = getRoomOffset();
+    canvas.drawLine(offset.topRight, offset.topLeft, paint);
+    canvas.drawLine(offset.topLeft, offset.bottomLeft, paint);
+    canvas.drawLine(offset.bottomLeft, offset.bottomRight, paint);
+  }
+
+  void _drawInnerWalls(Canvas canvas, Paint paint) {
+    var offset = getRoomOffset();
+    offset.innerOffset = 10;
+    canvas.drawLine(offset.topInnerRight, offset.topInnerLeft, paint);
+    canvas.drawLine(offset.topInnerLeft, offset.bottomInnerLeft, paint);
+    canvas.drawLine(offset.bottomInnerLeft, offset.bottomInnerRight, paint);
+  }
+
+  void _drawRightWall(Canvas canvas, Paint paint) {
+    var offset = getRoomOffset();
+    var doorLength = getDoorLength();
+    var scaledSideB = getScaledSideB();
+    // Drawing 4 wall with space for the door
+    canvas.drawLine(
+        offset.topRight,
+        Offset(
+            offset.leftEnd, offset.topStart + (scaledSideB - doorLength) * .5),
+        paint);
+    canvas.drawLine(
+        Offset(offset.leftEnd, offset.topEnd - (scaledSideB - doorLength) * .5),
+        offset.bottomRight,
+        paint);
+  }
+
+  void _drawRightInnerWall(Canvas canvas, Paint paint) {
+    var offset = getRoomOffset();
+    var doorLength = getDoorLength();
+    var scaledSideB = getScaledSideB();
+    offset.innerOffset = 10;
+    // Drawing 4 wall with space for the door
+    var doorTopStart = offset.topStart + (scaledSideB - doorLength) * .5;
+    var doorTopEnd = offset.topEnd - (scaledSideB - doorLength) * .5;
+
+    canvas.drawLine(
+        offset.topInnerRight, Offset(offset.leftInnerEnd, doorTopStart), paint);
+    canvas.drawLine(Offset(offset.leftInnerEnd, doorTopEnd),
+        offset.bottomInnerRight, paint);
+  }
+
+  void _drawConnectInnerWalls(Canvas canvas, Paint paint) {
+    var offset = getRoomOffset();
+    var doorLength = getDoorLength();
+    var scaledSideB = getScaledSideB();
+    offset.innerOffset = 10;
+
+    var doorTopStart = offset.topStart + (scaledSideB - doorLength) * .5;
+    var doorTopEnd = offset.topEnd - (scaledSideB - doorLength) * .5;
+
+    canvas.drawLine(Offset(offset.leftInnerEnd, doorTopStart),
+        Offset(offset.leftEnd, doorTopStart), paint);
+
+    canvas.drawLine(Offset(offset.leftInnerEnd, doorTopEnd),
+        Offset(offset.leftEnd, doorTopEnd), paint);
+  }
+
+  void _drawDoort(Canvas canvas, Paint paint) {
+    var offset = getRoomOffset();
+    var doorLength = getDoorLength();
+    var scaledSideB = getScaledSideB();
+    offset.innerOffset = 10;
+
+    var doorTopStart = offset.topStart + (scaledSideB - doorLength) * .5;
+    var doorTopEnd = offset.topEnd - (scaledSideB - doorLength) * .5;
+
+    // canvas.drawLine(Offset(offset.leftInnerEnd, doorTopStart),
+    //     Offset(offset.leftEnd, doorTopStart), paint);
+
+    // canvas.drawLine(Offset(offset.leftInnerEnd, doorTopEnd),
+    //     Offset(offset.leftEnd, doorTopEnd), paint);
+
+    var closedDoorPaint = Paint()
+      ..color = Colors.grey
+      ..strokeWidth = 1;
+
+    canvas.save();
+    canvas.translate(offset.leftEnd, doorTopStart); // door pivot point
+    canvas.drawLine(Offset(-offset.innerOffset, 0),
+        Offset(-offset.innerOffset, doorLength), closedDoorPaint);
+    canvas.rotate(pi * -.05); //9 deg,
+    canvas.drawLine(Offset(0, 0), Offset(0, doorLength), paint);
+
+    canvas.restore();
+  }
+
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint()
       ..color = Colors.black
-      ..strokeWidth = 3;
+      ..strokeWidth = 2;
 
     var guideLines = Paint()
       ..color = Colors.black
@@ -47,18 +250,12 @@ class RoomPainter extends CustomPainter {
     var topStart = (canvasHeight - scaledSideB) * .5;
     var topEnd = topStart + scaledSideB;
 
-    // Drawing 3 complete walls
-    canvas.drawLine(
-        Offset(leftStart, topStart), Offset(leftStart, topEnd), paint);
-    canvas.drawLine(
-        Offset(leftStart, topStart), Offset(leftEnd, topStart), paint);
-    canvas.drawLine(Offset(leftStart, topEnd), Offset(leftEnd, topEnd), paint);
-
-    // Drawing 4 wall with space for the door
-    canvas.drawLine(Offset(leftEnd, topStart),
-        Offset(leftEnd, topStart + (scaledSideB - doorLength) * .5), paint);
-    canvas.drawLine(Offset(leftEnd, topEnd - (scaledSideB - doorLength) * .5),
-        Offset(leftEnd, topEnd), paint);
+    _drawOuterWalls(canvas, paint);
+    _drawInnerWalls(canvas, paint);
+    _drawRightWall(canvas, paint);
+    _drawRightInnerWall(canvas, paint);
+    _drawConnectInnerWalls(canvas, paint);
+    _drawDoort(canvas, paint);
 
     // Guidelines
     canvas.drawLine(Offset(leftStart, topEnd + 10),
